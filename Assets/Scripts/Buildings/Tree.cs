ba02f43a48350da11bd49a5d7aef49944fbc4d8c
Quentin Musy <quentin@kinfoapp.com>
2018-09-28 19:39:50 +0100

    added forgotten stuff

---
+++ Assets/Scripts/Buildings/Tree.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Tree : Building {
+
+    public Tree()
+    {
+        this.BuildingType = BuildingType.TREE;
+        this.cost = 0;
+    }
+}
